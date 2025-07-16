dc95625f7cfe198d3ee6bd63a177182f3d12d62c
ICICLE Edge Admin <icicle.edge.admin>
2025-07-15 20:28:18 -0400

    VYP: Edited SSH

--- install.sh
+++ install.sh
@@ -120,4 +120,19 @@ pip3 install fastapi
 pip3 install py-lz4framed
 sudo apt-get -y install python3-softwarepilot
 
+KEY_SRC="/home/icicle/icicleEdge/Harmona/creds/"
+KEY_DEST="/home/icicle/.ssh/"
+KEY_FILE="stage"
+CONFIG="$KEY_DEST/config"
+
+mkdir -p "$KEY_DEST"
+cp "$KEY_SRC/$KEY_FILE" "$KEY_DEST/"
+chmod 600 "$KEY_DEST/$KEY_FILE"
+
+touch "$CONFIG"
+grep -qF "$KEY_FILE" "$CONFIG" || {
+  echo -e "\nHost *\n    IdentityFile $KEY_DEST/$KEY_FILE" >> "$CONFIG"
+}
+
+
 
